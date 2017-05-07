382960b83d1b56db4ea909fc5e2399522efbe8c6
Deng <dengfeifei027@163.com>
2017-05-08 01:01:05 +0800

    解决手机浏览器内容溢出

--- JS/home.js
+++ JS/home.js
@@ -89,11 +89,11 @@ $(function () {
                 }
             };
             $('body').swipe({
-                down: function () {
+                up: function () {
                     num = ++num < 8 ? num : 0;
                     sliding(DIV, ICON, num, "up");
                 },
-                up: function () {
+                down: function () {
                     num = --num >= 0 ? num : 7;
                     sliding(DIV, ICON, num, "down");
                 }
